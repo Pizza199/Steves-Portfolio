8089b6812ea732fdc558057feac60a2f37f871b6
Pizza199 <steveha999@gmail.com>
2025-03-25 05:48:38 -0500

    portfolio update

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -2,7 +2,10 @@ import "../index.css";
 import Project from "../components/Project";
 import SleepTrackerImage from "../images/SleepTracker.png";
 import WeatherBlitzImage from "../images/WeatherBlitz.png";
-import CandyStoreImage from "../images/CandyStore.jpg";
+import BookImage from "../images/Book.jpg";
+import SocialImage from "../images/Social.jpg";
+import VehicleImage from "../images/Vehicle.avif";
+import EmployeeImage from "../images/Employee.jpg";
 
 function Portfolio() {
     const projects = [
@@ -19,10 +22,28 @@ function Portfolio() {
             github: "https://example.com/project2",
         },
         {
-            title: "Candy Store",
-            image: CandyStoreImage,
-            deployment: "https://example.com/project3-deployment",
-            github: "https://example.com/project3",
+            title: "Book Search Engine",
+            image: BookImage,
+            deployment: "https://github.com/Pizza199/BookSearchEngine.git",
+            github: "https://github.com/Pizza199/BookSearchEngine.git",
+        },
+        {
+            title: "Social Network API",
+            image: SocialImage,
+            deployment: "https://github.com/Pizza199/Social-Network-API.git",
+            github: "https://github.com/Pizza199/Social-Network-API.git",
+        },
+        {
+            title: "Vehicle Builder",
+            image: VehicleImage,
+            deployment: "https://github.com/Pizza199/VehicleBuilder.git",
+            github: "https://github.com/Pizza199/VehicleBuilder.git",
+        },
+        {
+            title: "Employee Tracker",
+            image: EmployeeImage,
+            deployment: "https://github.com/Pizza199/Employee-Tracker.git",
+            github: "https://github.com/Pizza199/Employee-Tracker.git",
         },
     ]
         return (
--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -32,3 +32,5 @@ function Resume() {
         </div>
     );
 }
+
+export default Resume;
